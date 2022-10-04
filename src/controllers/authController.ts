0d2ee0bae5f8df3462d23c43a5f64e2b38af3d52
André Zopolato <andrezopo@gmail.com>
2022-10-03 23:09:56 -0300

    :sparkles: feat: stablish sign-up and sign-in functions with jwt token implementation

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
+import { User, SignInUser } from "../types/userType";
+import * as authService from "../services/authService";
 
 export async function signUp(req: Request, res: Response) {
-  res.status(200).send("Rota de cadastro");
+  const user: User = req.body;
+  await authService.signUp(user);
+  res.status(201).send("User created successfully");
 }
 
 export async function signIn(req: Request, res: Response) {
-  res.status(200).send("Rota de login");
+  const user: SignInUser = req.body;
+  const { token, name } = await authService.signIn(user);
+
+  res.status(200).send({ email: user.email, token, name });
 }

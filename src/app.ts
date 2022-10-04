03b6dafcb1779e3a8c91bcc01f31de8c33065628
André Zopolato <andrezopo@gmail.com>
2022-10-03 23:27:33 -0300

    :bug: fix: includes async-error import in app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors";
 import cors from "cors";
 import router from "./routers/router";
 import errorHandler from "./middlewares/errorHandler";

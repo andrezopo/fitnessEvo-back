3a01034ac8a614e70d6b2961302ffbcbdd04a852
André Zopolato <andrezopo@gmail.com>
2022-10-03 23:07:45 -0300

    :wrench: chore: create signInSchema and update signUpSchema to match password regex

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,10 +1,9 @@
 import joi from "joi";
-import { User } from "../types/userType";
+import { SignInUser } from "../types/userType";
 
 const userRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
 
-const signUpSchema = joi.object<User>({
+const signInSchema = joi.object<SignInUser>({
   email: joi.string().email().required(),
   password: joi.string().pattern(userRegex).required(),
-  name: joi.string().required(),
 });

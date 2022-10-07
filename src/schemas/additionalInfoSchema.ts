d50a72f335f6998118bdbe33135685149b6c2b84
André Zopolato <andrezopo@gmail.com>
2022-10-06 22:32:35 -0300

    :sparkles: feat: create additional info schema to use in middleware

---
+++ src/schemas/additionalInfoSchema.ts
@@ -0,0 +1,30 @@
+import joi from "joi";
+import { UserAdditionalInfos } from "../types/userType";
+
+const additionalInfoSchema = joi.object<UserAdditionalInfos>({
+  sex: joi.string().valid("male", "female").required(),
+  age: joi.number().integer().min(1).required(),
+  weight: joi.number().integer().min(1).required(),
+  height: joi.number().integer().min(1).required(),
+  activityLevel: joi
+    .string()
+    .valid(
+      "sedentary",
+      "lightly_active",
+      "active",
+      "very_active",
+      "extremely_active"
+    )
+    .required(),
+  trainingExperience: joi
+    .string()
+    .valid("begginer", "intermediate", "advanced", "atlhete")
+    .required(),
+  objective: joi
+    .string()
+    .valid("fat_loss", "maintenance", "mass_gain")
+    .required(),
+  bodyFat: joi.number().integer().min(1),
+});
+
+export default additionalInfoSchema;
